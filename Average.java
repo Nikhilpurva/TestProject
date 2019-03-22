b27a632f315fc5f68c21495a96e2775d86c1773e
Nikhil Purva <nikhil.purva@intimetec.com>
2019-03-22 17:10:23 +0530

    New file added

---
+++ Average.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Average
+{
+	public static void main(String args[])
+	{
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	int c = scan.nextInt();
+	double d = (a + b + c) / 3;
+	System.out.println("" + d);
+	}
+}
